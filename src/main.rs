405cb6be707272f3de549b7ffef20557c0f4448f
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2024-11-27 09:54:56 +0900

    ex1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use polars::prelude::*;
+
+pub fn main() {
+    let df = CsvReadOptions::default()
+        .try_into_reader_with_file_path(Some("input/titanic3.csv".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+    let df2 = CsvReadOptions::default()
+        .try_into_reader_with_file_path(Some("input/data1.csv".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+    let df3 = CsvReadOptions::default()
+        .try_into_reader_with_file_path(Some("input/data1_2.csv".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+    let df3 = CsvReadOptions::default()
+        .try_into_reader_with_file_path(Some("input/data1_3.csv".into()))
+        .unwrap()
+        .finish()
+        .unwrap();
+    println!("{}", df.head(Some(5)));
 }
